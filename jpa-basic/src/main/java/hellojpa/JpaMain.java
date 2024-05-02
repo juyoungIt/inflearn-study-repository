ee9561d9e9d0a19179cd5ee5b8956b8118b1d5a1
juyoungha <htj3460@gmail.com>
2024-05-02 13:27:08 +0900

    JPA-Basic[Section8-1] : 프록시

--- jpa-basic/src/main/java/hellojpa/JpaMain.java
+++ jpa-basic/src/main/java/hellojpa/JpaMain.java
@@ -2,8 +2,6 @@ package hellojpa;
 
 import jakarta.persistence.*;
 
-import java.util.List;
-
 public class JpaMain {
     public static void main(String[] args) {
         /* 애풀리케이션 로딩 시점에 딱 한 개만 만들어져야 함 */
@@ -17,18 +15,42 @@ public class JpaMain {
         /* 하나의 Transaction 내에서 처리 */
         try {
 
-            Movie movie = new Movie();
-            movie.setDirector("ryan");
-            movie.setDirector("choonsik");
-            movie.setName("도도도 춘식이");
-            movie.setPrice(10_000);
+            Member member = new Member();
+            member.setName("Choonsik");
 
-            em.persist(movie);
+            em.persist(member);
 
             em.flush();
             em.clear();
 
-            Movie findMovie = em.find(Movie.class, movie.getId());
+            // Member findMember = em.find(Member.class, member.getId());
+            Member findMember = em.getReference(Member.class, member.getId());
+            System.out.println("findMember before = " + findMember.getClass()); // Hibernate가 만든 프록시 클래스
+            System.out.println("findMember.id = " + findMember.getId());
+            System.out.println("findMember.name = " + findMember.getName());
+            /* 프록시 객체는 내부에 target entity 값만 업데이트하고, 그 자체가 교체되지는 않는다. */
+            System.out.println("findMember after = " + findMember.getClass()); // Hibernate가 만든 프록시 클래스
+
+            /* 타입 비교 시 주의할 점 */
+            Member memberA = new Member();
+            memberA.setName("Ryan");
+            em.persist(memberA);
+
+            Member memberB = new Member();
+            memberB.setName("Choonsik");
+            em.persist(memberB);
+
+            em.flush();
+            em.clear();
+
+            /* 하나는 em.find()로 하나는 em.reference() 로 조회한다 */
+            Member memberAEntity = em.find(Member.class, memberA.getId());
+            Member memberBProxy = em.getReference(Member.class, memberB.getId());
+
+            System.out.println("memberAEntity == memberBProxy : " + (memberAEntity.getClass() == memberBProxy.getClass()));
+            System.out.println("memberBProxy instanceof Member : " + (memberBProxy instanceof Member));
+
+            /* em.find() 도 프록시를 반환할 수 있다. */
 
             tx.commit(); // Transaction 내 변경사항을 커밋함
         } catch (Exception e) {
